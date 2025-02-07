3bc0d1633f0098ee265b503edad6be83250314b4
kaoi <kaoi@gft.com>
2025-02-06 22:38:31 -0300

    Implementa logia/funcionalidade do metodo sendEmail em EmailServices

--- microsservicos/email-ms/src/main/java/com/ms/email/services/EmailService.java
+++ microsservicos/email-ms/src/main/java/com/ms/email/services/EmailService.java
@@ -1,9 +1,15 @@
 package com.ms.email.services;
 
+import com.ms.email.enums.StatusEmail;
 import com.ms.email.models.EmailModel;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import com.ms.email.repositories.EmailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
@@ -11,7 +17,24 @@ public class EmailService {
     @Autowired
     EmailRepository emailRepository;
 
-    public void sendEmail(EmailModel emailModel) {
-        
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public EmailModel sendEmail(EmailModel emailModel) {
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        } catch (MailException e){
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            return emailRepository.save(emailModel);
+        }
     }
 }
