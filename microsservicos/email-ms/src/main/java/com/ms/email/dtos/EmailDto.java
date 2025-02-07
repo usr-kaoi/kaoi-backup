4fffaca1f8aba10c3020e925a655fb616415b63a
kaoi <kaoi@gft.com>
2025-02-07 11:06:05 -0300

    Retorna com as validacoes e adiciona getter e setters do DTO (nao sei pq ainda tento usar lombok)

--- microsservicos/email-ms/src/main/java/com/ms/email/dtos/EmailDto.java
+++ microsservicos/email-ms/src/main/java/com/ms/email/dtos/EmailDto.java
@@ -11,9 +11,11 @@ public class EmailDto {
     private String ownerRef;
 
     @NotBlank
+    @Email
     private String emailFrom;
 
     @NotBlank
+    @Email
     private String emailTo;
 
     @NotBlank
@@ -21,4 +23,44 @@ public class EmailDto {
 
     @NotBlank
     private String text;
+
+    public String getOwnerRef() {
+        return ownerRef;
+    }
+
+    public void setOwnerRef(String ownerRef) {
+        this.ownerRef = ownerRef;
+    }
+
+    public String getEmailFrom() {
+        return emailFrom;
+    }
+
+    public void setEmailFrom(String emailFrom) {
+        this.emailFrom = emailFrom;
+    }
+
+    public String getEmailTo() {
+        return emailTo;
+    }
+
+    public void setEmailTo(String emailTo) {
+        this.emailTo = emailTo;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
