e7c68370faa1cd10313673a2aa6c32165fb8d76c
kaoi <kaoi@gft.com>
2025-02-07 10:31:38 -0300

    teste mailtrap

---
+++ microsservicos/email-springboot/src/main/java/com/email_springboot/EmailSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.email_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSpringbootApplication.class, args);
+	}
+
+}
