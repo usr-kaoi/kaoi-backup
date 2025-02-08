c82a3e75d7691e383ceeb14052d4390920b5cddf
kaoi <kaoi@gft.com>
2025-02-07 23:20:27 -0300

    Adiciona metodos findall e findById em EmailService

--- microsservicos/email-ms/src/main/java/com/ms/email/services/EmailService.java
+++ microsservicos/email-ms/src/main/java/com/ms/email/services/EmailService.java
@@ -4,12 +4,16 @@ import com.ms.email.enums.StatusEmail;
 import com.ms.email.models.EmailModel;
 import com.ms.email.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class EmailService {
@@ -36,5 +40,13 @@ public class EmailService {
         } finally {
             return emailRepository.save(emailModel);
         }
+    }
+
+    public Page<EmailModel> findAll(Pageable pageable) {
+        return  emailRepository.findAll(pageable);
+    }
+
+    public Optional<EmailModel> findById(UUID emailId) {
+        return emailRepository.findById(emailId);
     }
 }
