6f6732a5aba4fcb16726de4edf28548fe58329a1
kaoi <kaoi@gft.com>
2025-02-07 23:31:48 -0300

    Adiciona anotacaa e tualiza metodo sendEmail

--- microsservicos/email-ms/src/main/java/com/ms/email/services/EmailService.java
+++ microsservicos/email-ms/src/main/java/com/ms/email/services/EmailService.java
@@ -3,6 +3,8 @@ package com.ms.email.services;
 import com.ms.email.enums.StatusEmail;
 import com.ms.email.models.EmailModel;
 import com.ms.email.repositories.EmailRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +12,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -18,15 +21,19 @@ import java.util.UUID;
 @Service
 public class EmailService {
 
+    Logger logger = LogManager.getLogger(EmailService.class);
+
     @Autowired
     EmailRepository emailRepository;
 
     @Autowired
     private JavaMailSender emailSender;
 
+    @Transactional
     public EmailModel sendEmail(EmailModel emailModel) {
-        emailModel.setSendDateEmail(LocalDateTime.now());
         try{
+            emailModel.setSendDateEmail(LocalDateTime.now());
+
             SimpleMailMessage message = new SimpleMailMessage();
             message.setFrom(emailModel.getEmailFrom());
             message.setTo(emailModel.getEmailTo());
@@ -35,10 +42,15 @@ public class EmailService {
             emailSender.send(message);
 
             emailModel.setStatusEmail(StatusEmail.SENT);
+            logger.info("Email sent successfully to: {} ", emailModel.getEmailTo());
         } catch (MailException e){
             emailModel.setStatusEmail(StatusEmail.ERROR);
+            logger.error("Email with error: {} ", emailModel.toString());
+            logger.error("Error {} ", e);
         } finally {
-            return emailRepository.save(emailModel);
+            emailModel = emailRepository.save(emailModel);
+            logger.info("Email saved successfully emailId: {} ", emailModel.getEmailId());
+            return emailModel;
         }
     }
 
