de1aa63a2942233b57becd0a8a0967bfe4a05cf8
kaoi <kaoi@gft.com>
2025-02-07 23:11:47 -0300

    Refatora endpoints e adiciona logs de erros

--- microsservicos/email-ms/src/main/java/com/ms/email/controllers/EmailController.java
+++ microsservicos/email-ms/src/main/java/com/ms/email/controllers/EmailController.java
@@ -3,27 +3,52 @@ package com.ms.email.controllers;
 import com.ms.email.dtos.EmailDto;
 import com.ms.email.models.EmailModel;
 import com.ms.email.services.EmailService;
-import jakarta.validation.Valid;
-import org.springframework.beans.BeanUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class EmailController {
 
+    Logger logger = LogManager.getLogger(EmailController.class);
+
     @Autowired
     EmailService emailService;
 
-    @PostMapping("/sending-email")
+    @PostMapping("/emails")
     public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto) {
-        EmailModel emailModel = new EmailModel();
-        BeanUtils.copyProperties(emailDto, emailModel);
-        emailService.sendEmail(emailModel);
-        return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+        return new ResponseEntity<>(emailService.sendEmail(emailDto.convertToEmailModel()), HttpStatus.CREATED);
     }
 
+    @GetMapping("/emails")
+    public ResponseEntity<Page<EmailModel>> getAllEmails(@PageableDefault(page = 0, size = 5, sort = "emailId", direction = Sort.Direction.DESC) Pageable pageable){
+        logger.trace("TRACE");
+        logger.debug("DEBUG");
+        logger.info("INFO");
+        logger.warn("WARN");
+        logger.error("ERROR");
+        logger.fatal("FATAL");
+        return new ResponseEntity<>(emailService.findAll(pageable), HttpStatus.OK);
+    }
+
+    @GetMapping("/emails/{emailId}")
+    public ResponseEntity<Object> getOneEmail(@PathVariable(value="emailId") UUID emailId){
+        Optional<EmailModel> emailModelOptional = emailService.findById(emailId);
+        if(!emailModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Email not found.");
+        }else {
+            return ResponseEntity.status(HttpStatus.OK).body(emailModelOptional.get());
+        }
+    }
 }
