de3945a1fe1a7e8addec766b80712de08cac966d
kaoi <kaoi@gft.com>
2025-02-17 20:12:13 -0300

    Implementa lógica dos métodos convertToDouble e isNumeric, com substituição de

--- microsservicos/erudio-microservice/api-rest-math/src/main/java/com/erudio/api_rest_math/MathController.java
+++ microsservicos/erudio-microservice/api-rest-math/src/main/java/com/erudio/api_rest_math/MathController.java
@@ -20,10 +20,15 @@ public class MathController {
     }
 
     private Double convertToDouble(String strNumber) {
+        if(strNumber == null) return 0D;
+        String number = strNumber.replaceAll(",", ".");
+        if(isNumeric(number)) return Double.parseDouble(number);
         return null;
     }
 
     private boolean isNumeric(String strNumber) {
-        return false;
+        if(strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 }
