a00bea97936d16f019a57d581715c2af17464e5c
kaoi <kaoi@gft.com>
2025-02-17 19:26:37 -0300

    Inicia api para calculos

---
+++ microsservicos/erudio-microservice/api-rest-math/src/main/java/com/erudio/api_rest_math/MathController.java
@@ -0,0 +1,17 @@
+package com.erudio.api_rest_math;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+
+    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double sum(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo) {
+        return 1D;
+    }
+}
