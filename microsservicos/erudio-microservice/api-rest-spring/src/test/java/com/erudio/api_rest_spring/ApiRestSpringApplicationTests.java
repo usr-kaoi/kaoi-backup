ffa0dbce5751ab4a920af9842e3cf123a1f06918
kaoi <kaoi@gft.com>
2025-02-17 11:50:19 -0300

    inicializando pratica curso microsservicos - Erudio

---
+++ microsservicos/erudio-microservice/api-rest-spring/src/test/java/com/erudio/api_rest_spring/ApiRestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erudio.api_rest_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
