80397190ffd821b7c4f278a3f67336cd10fa0ac8
kaoi <kaoi@gft.com>
2025-02-18 13:29:27 -0300

    Inicia refatoração de pacotes para aplicar os conceitos de API Rest. Cria classe NumberConverter para transferir a lógica de conversão (String to Doule) e o boolean que checa se a String recebida é equivalente a um Double

--- microsservicos/erudio-microservice/api-rest-math/src/main/java/com/erudio/api_rest_math/MathController.java
+++ microsservicos/erudio-microservice/api-rest-math/src/main/java/com/erudio/api_rest_math/MathController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.erudio.api_rest_math.converters.NumberConverter.*;
+
 @RestController
 public class MathController {
 
@@ -72,18 +74,5 @@ public class MathController {
         }
         double rootNumberOne = Math.sqrt(convertToDouble(numberOne));
         return rootNumberOne;
-    }
-
-    private Double convertToDouble(String strNumber) {
-        if(strNumber == null) return 0D;
-        String number = strNumber.replaceAll(",", ".");
-        if(isNumeric(number)) return Double.parseDouble(number);
-        return null;
-    }
-
-    private boolean isNumeric(String strNumber) {
-        if(strNumber == null) return false;
-        String number = strNumber.replaceAll(",", ".");
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 }
---
+++ microsservicos/erudio-microservice/api-rest-math/src/main/java/com/erudio/api_rest_math/converters/NumberConverter.java
@@ -0,0 +1,17 @@
+package com.erudio.api_rest_math.converters;
+
+public class NumberConverter {
+
+    public static Double convertToDouble(String strNumber) {
+        if(strNumber == null) return 0D;
+        String number = strNumber.replaceAll(",", ".");
+        if(isNumeric(number)) return Double.parseDouble(number);
+        return null;
+    }
+
+    public static boolean isNumeric(String strNumber) {
+        if(strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+}
