a1431b315e0d99b82e065fab3e5652b0202dd1b9
kaoi <kaoi@gft.com>
2025-02-17 21:50:17 -0300

    Resolução desafio: Implementa metodos com demais operações (Subtração, Multiplicação, Divisão, Média e Raiz Quadrada), em MathController

--- microsservicos/erudio-microservice/api-rest-math/src/main/java/com/erudio/api_rest_math/MathController.java
+++ microsservicos/erudio-microservice/api-rest-math/src/main/java/com/erudio/api_rest_math/MathController.java
@@ -15,9 +15,63 @@ public class MathController {
             @PathVariable(value = "numberTwo") String numberTwo)  throws Exception {
 
         if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
-            throw new UnsupportedMathOperationException();
+            throw new UnsupportedMathOperationException("Please, insert a numeric value!");
         }
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/difference/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double difference(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo)  throws Exception {
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please, insert a numeric value!");
+        }
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/product/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double product(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo)  throws Exception {
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please, insert a numeric value!");
+        }
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/quotient/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double quotient (
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo)  throws Exception {
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please, insert a numeric value!");
+        }
+        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/avarage/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double avarage(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo)  throws Exception {
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please, insert a numeric value!");
+        }
+        return (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2;
+    }
+
+    @RequestMapping(value = "/root/{numberOne}", method=RequestMethod.GET)
+    public Double root( @PathVariable(value = "numberOne") String numberOne) throws Exception {
+
+        if(!isNumeric(numberOne)){
+            throw new UnsupportedMathOperationException("Please, insert a numeric value!");
+        }
+        double rootNumberOne = Math.sqrt(convertToDouble(numberOne));
+        return rootNumberOne;
     }
 
     private Double convertToDouble(String strNumber) {
--- microsservicos/erudio-microservice/api-rest-math/src/main/java/com/erudio/api_rest_math/exceptions/UnsupportedMathOperationException.java
+++ microsservicos/erudio-microservice/api-rest-math/src/main/java/com/erudio/api_rest_math/exceptions/UnsupportedMathOperationException.java
@@ -7,4 +7,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class UnsupportedMathOperationException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
+
+    public UnsupportedMathOperationException(String ex) {
+        super(ex);
+    }
 }
