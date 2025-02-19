6413b1d657252cb51725f8f9be2b13953631dc2b
kaoi <kaoi@gft.com>
2025-02-18 21:50:15 -0300

    Fix: erro de syntax na linha 13 em services/PersonService e nome do projeto

--- microsservicos/erudio-microservice/api-rest-person/src/main/java/com/erudio/api_rest_person/services/PersonService.java
+++ microsservicos/erudio-microservice/api-rest-person/src/main/java/com/erudio/api_rest_person/services/PersonService.java
@@ -1,16 +1,16 @@
 package com.erudio.api_rest_person.services;
 
 import com.erudio.api_rest_person.model.Person;
-import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
 
 @Service
 public class PersonService {
 
     private final AtomicLong counter = new AtomicLong();
-    private Logger logger = logger.getLogger(PersonService.class.getName());
+    private Logger logger = Logger.getLogger(PersonService.class.getName());
 
     public Person findById(String id) {
 
